4f3ec7f5507fdf2033dc43f0174ef35d2af3fc4f
Margaret Berry <margaretshelaghmcgovern@gmail.com>
2017-09-29 08:59:28 -0700

    update database to include join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170929152156) do
+ActiveRecord::Schema.define(version: 20170929155451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170929152156) do
   create_table "brands", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "brands_stores", force: :cascade do |t|
+    t.integer "brand_id"
+    t.integer "store_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
