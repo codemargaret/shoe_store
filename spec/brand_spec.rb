bcab3bfb408105f959efae902c50c48c65e8b00b
Margaret Berry <margaretshelaghmcgovern@gmail.com>
2017-09-29 13:06:04 -0700

    add validations so that the user cannot enter brands or stores that are blank or more than 100 characters

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe(Brand) do
   it { should have_and_belong_to_many :stores }
+
+  it 'validates presence of name' do
+    brand = Brand.new({:name => ""})
+    expect(brand.save).to eq false
+  end
+
+  it("only accepts input that is at most 100 characters") do
+    brand = Brand.new({:name => "a".*(101)})
+    expect(brand.save).to eq false
+  end
 end
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe(Store) do
   it { should have_and_belong_to_many :brands }
+
+  it 'validates presence of name' do
+    store = Store.new({:name => ""})
+    expect(store.save).to eq false
+  end
+
+  it("only accepts input that is at most 100 characters") do
+    store = Store.new({:name => "a".*(101)})
+    expect(store.save).to eq false
+  end
 end
